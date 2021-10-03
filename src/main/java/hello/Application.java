5f6a97345a099c7477b937980bd51af8ac284173
Derrick Wong <derrick.chwong@gmail.com>
2021-10-03 09:33:56 +0800

    print info

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,16 +1,21 @@
 package hello;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@Slf4j
 public class Application {
 
   static class Self {
@@ -27,6 +32,10 @@ public class Application {
     public String direction;
     public Boolean wasHit;
     public Integer score;
+  }
+
+  class Coordination {
+    public Integer x,y;
   }
 
   static class Arena {
@@ -53,13 +62,21 @@ public class Application {
     return "Let the battle begin!";
   }
 
+  // private Coordination myLocation(ArenaUpdate arenaUpdate){
+  //   arenaUpdate.arena.state.get
+  // }
+
   @PostMapping("/**")
   public String index(@RequestBody ArenaUpdate arenaUpdate) {
     System.out.println(arenaUpdate);
-    // String[] commands = new String[]{"F", "R", "L", "T"};
-    // int i = new Random().nextInt(4);
-    // return commands[i];
-    return "T";
+    log.info("dimension (?,?)", arenaUpdate.arena.dims.get(0), arenaUpdate.arena.dims.get(1));
+    PlayerState myState = arenaUpdate.arena.state.get("https://java-springboot-bot-o6vtvlku4q-as.a.run.app");
+    log.info("my coordination (?,?)", myState.x, myState.y );
+
+    String[] commands = new String[]{"F", "R", "L", "T"};
+    int i = new Random().nextInt(4);
+    return commands[i];
+    // return "F";
   }
 
 }
